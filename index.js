935fe6972c9bdc80d33caf8071e68aa30939b476
Reggenius <reginaldchikelu@gmail.com>
2024-07-20 10:01:09 +0100

    Include the converter project in the rendered webpage

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const projURL = ["03 password-generator/password-generator.html", "02 basketball-scoreboard/basketball-scoreboard.html", "01 passenger-counter-app/passenger-counter-app.html"];
+const projURL = ["03 password-generator/password-generator.html", "02 basketball-scoreboard/basketball-scoreboard.html", "04 unit-converter/unit-converter.html", "01 passenger-counter-app/passenger-counter-app.html"];
+let converterImg = document.querySelector("#converter img");
 
 function redirectToProj(arrIndex) {
     window.location.href = projURL[arrIndex];
 }
+
+function editImgUrl(el, url) {
+    converterImg.setAttribute("src", url);
+}
+
+if (window.innerWidth <= 1100) {
+    editImgUrl(converterImg, "images/converter1.png");
+}
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth <= 1100) {
+        editImgUrl(converterImg, "images/converter1.png");
+    }
+    else {
+        editImgUrl(converterImg, "images/converter.png");
+    }
+});
