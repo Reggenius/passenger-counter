6d3a668b5a25180494d3ad4dfc77d75a71126e99
Reggenius <reginaldchikelu@gmail.com>
2024-08-04 15:17:43 +0100

    Modify the pages url array

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const projURL = [
     "03 password-generator/password-generator.html", 
     "02 basketball-scoreboard/basketball-scoreboard.html", 
-    "04 unit-converter/unit-converter.html", 
+    "05 unit-converter/unit-converter.html", 
     "01 passenger-counter-app/passenger-counter-app.html", 
     "https://github.com/Reggenius/techup-pre_course/tree/main/04%20chrome-extension",
     "https://github.com/Reggenius/techup-pre_course/tree/main/06%20mobile%20app"
